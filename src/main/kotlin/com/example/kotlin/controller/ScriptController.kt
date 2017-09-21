5c410a060c901eb01f446f0592cacb469458f1a8
Maksym Adamek <maksym100@gmail.com>
2017-09-21 20:43:24 +0200

    not working version of mutli script

--- src/main/kotlin/com/example/kotlin/controller/ScriptController.kt
+++ src/main/kotlin/com/example/kotlin/controller/ScriptController.kt
@@ -4,9 +4,11 @@ import com.example.kotlin.provider.ScriptProvider
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.thymeleaf.TemplateEngine
 import org.thymeleaf.context.Context
+import java.util.stream.Collectors
 
 /**
  * @author
@@ -20,5 +22,15 @@ class ScriptController @Autowired constructor(val scriptProvider: ScriptProvider
         val ctx = Context()
         ctx.setVariable("scriptBody", scriptContent)
         return template.process("testTemplate", ctx)
+    }
+
+    @GetMapping("/scripts")
+    fun getCombineScripts(@RequestParam("partners") partners: List<String>): String {
+        partners.stream().map(scriptProvider::getScriptContent).collect(Collectors.toList())
+
+        val scripts = partners.map { partner: String -> scriptProvider.getScriptContent(partner) }
+        val ctx = Context()
+        ctx.setVariable("scriptBodies", scripts)
+        return template.process("multiTemplate", ctx)
     }
 }
